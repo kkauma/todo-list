a7345200039c4c0557226613fd5f4d00050a8478
kkauma <kylekauma@gmail.com>
2022-09-07 19:27:48 -0500

    write logic for when user quits the application

--- todo.py
+++ todo.py
@@ -10,14 +10,35 @@ print("""
 
 todos = []
 counter = 1
+skip_input = ["h", "q"]
+completed_todos = []
+
 while True:
     print("*" * 35)
-    user_input = input("Enter a command. Type 'h' for help:\n> ")
+    user_input = input("Enter a command. Type 'h' for help:\n> ").lower()
+    if user_input == "h":
+        print("TODO LIST HELP")
+        print("Type 'q' to quit")
+        print("To add a todo to the list, type it and hit enter")
+        print("To complete a todo enter its number")
+
+    if user_input not in skip_input or user_input 
     todos.append(f"{counter}) {user_input}")
     counter += 1
 
     for todo in todos:
         print(todo)
+
+    # Print out completed todos when user quits application
+    if user_input == "q":
+        print(f"Today you completed {len(completed_todos)} todos:\n")
+        for todo in completed_todos:
+            print(f"* {todo}")
+
+        print("Nice work!")
+
+    
+
     
 
 
