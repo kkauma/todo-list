57e743cc78615ec913a5d383474ca4306faab8ce
kkauma <kylekauma@gmail.com>
2022-09-07 19:05:20 -0500

    create inital template and ask for user input

---
+++ todo.py
@@ -0,0 +1,14 @@
+# Todo List App
+
+print("""
+  _____         _           
+ |_   _|__   __| | ___  ___ 
+   | |/ _ \ / _` |/ _ \/ __|
+   | | (_) | (_| | (_) \__ \\
+   |_|\___/ \__,_|\___/|___/
+""")
+
+print("*" * 30)
+
+user_input = input("Enter a command. Type 'h' for help: ")
+
