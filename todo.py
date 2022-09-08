e38d39e6cc595b55adcfd57a27331897f4bb3ee8
kkauma <kylekauma@gmail.com>
2022-09-07 20:27:03 -0500

    refactor code and finish app

--- todo.py
+++ todo.py
@@ -9,33 +9,35 @@ print("""
 """)
 
 todos = []
-counter = 1
-skip_input = ["h", "q"]
-completed_todos = []
-
+completed = []
 while True:
-    print("*" * 35)
-    user_input = input("Enter a command. Type 'h' for help:\n> ").lower()
-    if user_input == "h":
+    for i in range(len(todos)):
+        print(f"{i+1}) {todos[i]}")
+        
+    print("***********************************")
+    print("Enter a command. Type 'h' for help:")
+    command = input("> ")
+    if command == "q":
+        break
+    elif command == "h":
         print("TODO LIST HELP")
         print("Type 'q' to quit")
         print("To add a todo to the list, type it and hit enter")
         print("To complete a todo enter its number")
-
-    if user_input not in skip_input or user_input 
-    todos.append(f"{counter}) {user_input}")
-    counter += 1
-
-    for todo in todos:
-        print(todo)
-
-    # Print out completed todos when user quits application
-    if user_input == "q":
-        print(f"Today you completed {len(completed_todos)} todos:\n")
-        for todo in completed_todos:
-            print(f"* {todo}")
-
-        print("Nice work!")
+    elif command.isnumeric():
+        idx = int(command) - 1
+        if idx >= len(todos):
+            print("THERE IS NO TODO WITH THAT NUMBER!")
+        else:
+            done_todo = todos.pop(idx)
+            completed.append(done_todo)
+    else: 
+        todos.append(command)
+    # Print todos from list
+if completed:
+    print(f"You completed {len(completed)} todos today: ")
+    for todo in completed:
+        print(f"* {todo}")
 
     
 
